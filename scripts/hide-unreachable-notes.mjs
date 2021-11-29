56f8c66cee0c8c7fae5e38344799921116ffa238
Martin <github@amusingtime.uk>
2021-11-29 11:37:29 +0000

    Provide buttons in NoteConfig window to control revealed state

--- scripts/hide-unreachable-notes.mjs
+++ scripts/hide-unreachable-notes.mjs
@@ -5,10 +5,12 @@
 import {libWrapper} from './libwrapper-shim.js'
 
 const MODULE_NAME = "realm-works-import";
-const PIN_IS_REVEALED = "pinIsRevealed";
-const NOTE_FLAG = `flags.${MODULE_NAME}.${PIN_IS_REVEALED}`;
-const PIN_COLOUR_LINKED     = '#7CFC00';
-const PIN_COLOUR_NOT_LINKED = '#c000c0';
+const USE_PIN_REVEALED = "usePinRevealed";
+const PIN_IS_REVEALED  = "pinIsRevealed";
+const FLAG_IS_REVEALED  = `flags.${MODULE_NAME}.${PIN_IS_REVEALED}`;
+const FLAG_USE_REVEALED = `flags.${MODULE_NAME}.${USE_PIN_REVEALED}`;
+const CONFIG_TINT_REACHABLE_LINK   = "tintReachableLink";
+const CONFIG_TINT_UNREACHABLE_LINK = "tintUnreachableLink";
 
 /**
  * Wraps the default Note#refresh to allow the visibility of scene Notes to be controlled by the reveal
@@ -19,6 +21,9 @@ const PIN_COLOUR_NOT_LINKED = '#c000c0';
  */
 function Note_refresh(wrapped, ...args) {
 	let result = wrapped(...args);
+	const use_reveal = result.document.getFlag(MODULE_NAME, USE_PIN_REVEALED);
+	if (use_reveal === undefined || !use_reveal) return result;
+	
 	const value = result.document.getFlag(MODULE_NAME, PIN_IS_REVEALED);
 	// Use the revealed state as the visibility of the Note.
 	// If the linked topic is not visible to the player then clicking will do nothing.
@@ -37,9 +42,14 @@ function Note_refresh(wrapped, ...args) {
  * @return [Note]    This Note
  */
 function Note_drawControlIcon(wrapped, ...args) {
+	const use_reveal = this.document.getFlag(MODULE_NAME, USE_PIN_REVEALED);
+	if (use_reveal === undefined || !use_reveal) return wrapped(...args);
+	
 	const value = this.document.getFlag(MODULE_NAME, PIN_IS_REVEALED);
 	if (value != undefined) {
-		this.data.iconTint = this.entry?.testUserPermission(game.user, "LIMITED") ? PIN_COLOUR_LINKED : PIN_COLOUR_NOT_LINKED;
+		const is_linked = this.entry?.testUserPermission(game.user, "LIMITED");
+		const colour = game.settings.get(MODULE_NAME, is_linked ? CONFIG_TINT_REACHABLE_LINK : CONFIG_TINT_UNREACHABLE_LINK);
+		if (colour?.length > 0) this.data.iconTint = colour;
 	}
 	return wrapped(...args);
 }
@@ -53,12 +63,12 @@ function Note_drawControlIcon(wrapped, ...args) {
  */
 export function setNoteRevealed(notedata,visible) {
 	// notedata might not exist as a Note, so setFlag is not available
-	setProperty(notedata, NOTE_FLAG, visible);
+	setProperty(notedata, FLAG_USE_REVEALED, true);
+	setProperty(notedata, FLAG_IS_REVEALED,  visible);
 	// Default tint based on GM view
-	notedata.iconTint = notedata.entryId ? PIN_COLOUR_LINKED : PIN_COLOUR_NOT_LINKED;
+	let tint = game.settings.get(MODULE_NAME, notedata.entryId ? CONFIG_TINT_REACHABLE_LINK : CONFIG_TINT_UNREACHABLE_LINK);
+	if (tint?.length > 0) notedata.iconTint = tint;
 }
-
-// TODO: Add option to Note editor window
 
 Hooks.once('canvasInit', () => {
 	// This is only required for Players, not GMs (game.user accessible from 'ready' event but not 'init' event)
@@ -66,4 +76,45 @@ Hooks.once('canvasInit', () => {
 		libWrapper.register(MODULE_NAME, 'Note.prototype.refresh',          Note_refresh,         libWrapper.WRAPPER);
 		libWrapper.register(MODULE_NAME, 'Note.prototype._drawControlIcon', Note_drawControlIcon, libWrapper.WRAPPER);
 	}
+})
+
+//
+// Update NoteConfig to handle REVEALED state
+//
+
+/**
+ * Update Note config window with a text box to allow entry of GM-text.
+ * Also replace single-line of "Text Label" with a textarea to allow multi-line text.
+ * @param {NoteConfig} app    The Application instance being rendered (NoteConfig)
+ * @param {jQuery} html       The inner HTML of the document that will be displayed and may be modified
+ * @param {object] data       The object of data used when rendering the application (from NoteConfig#getData)
+ */
+Hooks.on("renderNoteConfig", async function (app, html, data) {
+	// Check box for REVEALED state
+	let checked = (data.document.getFlag(MODULE_NAME, PIN_IS_REVEALED) ?? true) ? "checked" : "";
+	let revealed_control = $(`<div class='form-group'><label>Revealed to Players</label><div class='form-fields'><input type='checkbox' name='${FLAG_IS_REVEALED}' ${checked}></div></div>`)
+	html.find("select[name='entryId']").parent().parent().after(revealed_control);
+	let use_reveal = (data.document.getFlag(MODULE_NAME, USE_PIN_REVEALED) ?? false) ? "checked" : "";
+	let mode_control = $(`<div class='form-group'><label>Use Reveal State</label><div class='form-fields'><input type='checkbox' name='${FLAG_USE_REVEALED}' ${use_reveal}></div></div>`)
+	html.find("select[name='entryId']").parent().parent().after(mode_control);
+})
+
+
+Hooks.once('init', () => {
+    game.settings.register(MODULE_NAME, CONFIG_TINT_REACHABLE_LINK, {
+		name: "Note Tint Colour when linked",
+		hint: "For players, the RGB value to be used to tint scene Notes if they have a reachable link (if left blank then the tint, if any, will remain unchanged).  For GMs, this is the initial Icon Tint set during import",
+		scope: "world",
+		type:  String,
+		default: '#7CFC00',
+		config: true,
+	});
+    game.settings.register(MODULE_NAME, CONFIG_TINT_UNREACHABLE_LINK, {
+		name: "Note Tint Colour when not linked",
+		hint: "For players, the RGB value to be used to tint scene Notes if they do not have a reachable link (if left blank then the tint, if any, will remain unchanged).  For GMs, this is the initial Icon Tint set during import",
+		scope: "world",
+		type:  String,
+		default: '#c000c0',
+		config: true,
+	});
 })
